323dd3359fa0d92d357d89b0e294060fc0a7fd20
Muhammed Fatih Bulut <bulutmf@gmail.com>
2014-03-25 12:40:03 -0400

    Added sorting package

---
+++ src/com/bulutmf/sorting/InsertionSort.java
@@ -0,0 +1,13 @@
+package com.bulutmf.sorting;
+
+public class InsertionSort {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
